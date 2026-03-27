fa82015b68e14b42bfb90160e017f50b9da0c851
michelekluck <michelenamur27@gmail.com>
2026-03-26 21:17:44 -0300

    adjust

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        files: ["**/*.{js,jsx,ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+        rules: {
+            semi: ["error", "always"],
+            quotes: ["error", "double"],
+        },
+    },
+];
